550a181ece2ad090cf336057b2830aa15564dcd8
Rob <kritz.rob@gmail.com>
2024-07-07 23:26:58 +0200

    match on data supplied

--- src/http.rs
+++ src/http.rs
@@ -17,28 +17,21 @@ struct RequestDetails<'a> {
 pub fn match_request(message: &str) -> String {
     let lines: Vec<&str> = message.split("\r\n").collect();
 
-    let res: String = match lines[0].split(" ").collect::<Vec<&str>>().as_slice() {
-        ["GET", path, "HTTP/1.1"] => map_err(RequestDetails {path: path, headers: None, data: None }, &handle_get),
-        ["DELETE", path, "HTTP/1.1"] => map_err(RequestDetails { path: path, headers: None, data: None }, &handle_delete),
-        // ["POST", path, "HTTP/1.1"] => map_err(RequestDetails { path: path, headers: None, data: None }, &handle_delete),
+    let res = match message.split("\r\n\r\n").collect::<Vec<&str>>().as_slice() {
+        [_request_params, ""] => match lines[0].split(" ").collect::<Vec<&str>>().as_slice() {
+            ["GET", path, "HTTP/1.1"] => map_err(RequestDetails {path, headers: None, data: None }, &handle_get),
+            ["DELETE", path, "HTTP/1.1"] => map_err(RequestDetails { path, headers: None, data: None }, &handle_delete),
+            _ => response::bad_request(None)
+        },
+        [_request_params, data] => match lines[0].split(" ").collect::<Vec<&str>>().as_slice() {
+            ["POST", path, "HTTP/1.1"] => map_err(RequestDetails { path, headers: None, data: Some(data.as_bytes().to_vec()) }, &handle_post),
+            ["PUT", path, "HTTP/1.1"] => map_err(RequestDetails { path, headers: None, data: Some(data.as_bytes().to_vec()) }, &handle_put),
+            _ => response::bad_request(None)
+        },
         _ => response::bad_request(None)
     };
 
     res
-
-
-    // let req: Option<Request> = match lines[0].split(" ").collect::<Vec<&str>>().as_slice() {
-    //     [method, path, "HTTP/1.1"] => Some(Request {method: method, path: path, headers: None, data: None }),
-    //     _ => None
-    // };
-
-    // match req {
-    //     Some(Request { method: "GET", path, .. }) => map_err(path, &handle_get),
-    //     Some(Request { method: "DELETE", path, .. }) => map_err(path, &handle_delete),
-    //     // Request { method: "GET", path, headers, data } => handle_get(path),
-    //     // Request { method: "GET", path, headers, data } => handle_get(path),
-    //     _ => response::bad_request(None)
-    // }
 }
 
 fn map_err(req: RequestDetails, f: &dyn Fn(RequestDetails) -> io::Result<String>) -> String {
@@ -64,13 +57,13 @@ fn handle_get(req: RequestDetails) -> io::Result<String> {
     Ok(response::ok(Some(&buffer)))
 }
 
-// fn handle_post(_path: &str, data: &Vec<u8>) -> io::Result<String> {
-//     Ok(response::ok(Some(&buffer)))
-// }
+fn handle_post(req: RequestDetails) -> io::Result<String> {
+    todo!()
+}
 
-// fn handle_put(_path: &str, data: &Vec<u8>) -> io::Result<String> {
-//     Ok(response::ok(Some(&buffer)))
-// }
+fn handle_put(req: RequestDetails) -> io::Result<String> {
+    todo!()
+}
 
 fn handle_delete(req: RequestDetails) -> io::Result<String> {
     let full_path = format!("{RESOURCES_FILE_PATH}{}", req.path);
